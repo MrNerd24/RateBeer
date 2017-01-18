19a3efdf889843af7d3467381b536978441b90b3
Juuso Lassila <juuso.lassila@helsinki.fi>
2017-01-18 11:31:52 +0200

    Viikko 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117094834) do
+ActiveRecord::Schema.define(version: 20170118072207) do
+
+  create_table "beers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "style"
+    t.integer  "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "breweries", force: :cascade do |t|
     t.string   "name"
